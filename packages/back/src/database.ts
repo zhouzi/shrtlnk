493ed6f9d2ed6558afc3a1511765ca9c5f5c37db
gabinaureche <hello@gabinaureche.com>
2021-03-31 16:14:03 +0200

    Fix database connection

--- packages/back/src/database.ts
+++ packages/back/src/database.ts
@@ -12,7 +12,7 @@ const pg = Knex({
   client: "pg",
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === "production",
+    ssl: { rejectUnauthorized: false },
   },
 });
 
